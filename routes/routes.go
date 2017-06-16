c5cb2f651de8905ed94c6ac98839cf19c83c6f06
Zeu Cxb <zeucxb@gmail.com>
2017-06-16 16:24:54 -0300

    Add temporary cors

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,25 @@
 package routes
 
-import "github.com/pressly/chi"
+import (
+	"github.com/goware/cors"
+	"github.com/pressly/chi"
+)
 
 // R is the router object
 var R = chi.NewRouter()
 
 func init() {
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	R.Use(cors.Handler)
+
 	searchRoute(R)
 }
