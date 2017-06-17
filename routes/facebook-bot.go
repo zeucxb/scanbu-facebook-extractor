13d697b2a64854c8fcdca72d3e518d4d43c10a96
Zeu Cxb <zeucxb@gmail.com>
2017-06-17 12:16:23 -0300

    Add facebook bot

--- routes/facebook-bot.go
+++ routes/facebook-bot.go
@@ -6,10 +6,6 @@ import (
 	"github.com/pressly/chi"
 )
 
-<<<<<<< HEAD
 func fbotRoute(route *chi.Mux) {
-=======
-func searchRoute(route *chi.Mux) {
->>>>>>> 1f1ef17b19fc79941108eaf7cf2fe321a479b32f
 	route.Get("/fbot", handlers.FacebookBot)
 }
