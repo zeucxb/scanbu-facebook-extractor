1b47aaff2b8440a380cc51f88fa48b8ba0c0cebd
Zeu Cxb <zeucxb@gmail.com>
2017-06-17 12:31:54 -0300

    Add facebook bot webhook response

--- modules/facebook-bot/handlers/facebook-bot.go
+++ modules/facebook-bot/handlers/facebook-bot.go
@@ -3,9 +3,20 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/pressly/chi/render"
 )
 
 // FacebookBot is the facebook bot webhook handler
 func FacebookBot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", "776250382")
+	query := r.URL.Query()
+
+	verifyToken := query.Get("hub.verify_token")
+
+	if verifyToken != "scanbu-S3cr3t" {
+		render.Status(r, http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", query.Get("hub.challenge"))
 }
