e8e8a14997332b2758e9787321de84236affb574
Zeu Cxb <zeucxb@gmail.com>
2017-06-17 18:15:08 -0300

    Add politica de privacidade

--- routes/facebook-bot.go
+++ routes/facebook-bot.go
@@ -1,12 +1,26 @@
 package routes
 
 import (
+	"io/ioutil"
+	"net/http"
 	"scanbu-api/modules/facebook-bot/handlers"
 
+	"fmt"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/pressly/chi"
 )
 
 func fbotRoute(route *chi.Mux) {
 	route.Get("/fbot", handlers.FacebookBot)
 	route.Post("/fbot", handlers.FacebookBotReceiver)
+
+	route.Get("/fb/politica/privacidade", func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadFile("politica-de-privacidade.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, "%s", b)
+	})
 }
